863ff1c952f6ceb3871335f2b71c32699b63289d
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-19 16:16:27 +0200

    test create table

--- tests.py
+++ tests.py
@@ -1,8 +1,7 @@
-from main import create_connection
+from main import create_connection, create_table
 from psycopg2.extensions import connection as pg
 
-import unittest
-import os
+import unittest, os, psycopg2
 
 class TestDB(unittest.TestCase):
 
@@ -32,6 +31,41 @@ class TestDB(unittest.TestCase):
 
         error_msg = "Could not connect to the database. Invalid credentials"
         self.assertEqual(str(context.exception), error_msg)
+
+    def test_failed_create_table(self):
+        db = create_connection(self.db_name, self.db_user, self.db_pass)
+        self.assertIsInstance(db, pg)
+
+        with db:
+
+            table = create_table(db, "pokemons")
+
+            with db.cursor() as cursor:
+
+                with self.assertRaises(psycopg2.ProgrammingError) as context:
+                    cursor.execute("SELECT * FROM pokemon;")
+
+                # 42P01 is the error code for "table [] does not exist"
+                self.assertEqual("42P01", context.exception.pgcode)
+
+    def test_create_table(self):
+        db = create_connection(self.db_name, self.db_user, self.db_pass)
+
+        query = """
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema='public'
+            AND table_type='BASE TABLE';
+        """
+
+        with db:
+            table = create_table(db, "pokemons")
+
+            with db.cursor() as cursor:
+                cursor.execute(query)
+                table_name = cursor.fetchone()
+
+            self.assertEqual("pokemons", table_name[0])
 
 if __name__ == "__main__":
     unittest.main()
