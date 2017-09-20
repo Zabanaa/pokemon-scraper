721dd9293689e30345fa53d4bafce883da780bf4
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-20 12:58:55 +0200

    create test http class, not found, server error and OK test methods

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from exceptions import RequestError
 from main import create_connection, create_table, clear_data, get_page, insert_to_db
 from psycopg2.extensions import connection as pg
 from unittest.mock import patch
@@ -146,6 +147,45 @@ class TestDBOperations(unittest.TestCase):
 
             error_message = "Unique constraint violated"
             self.assertIn(error_message, str(context.exception))
+
+class TestHTTP(unittest.TestCase):
+
+    def test_get_page_success(self):
+
+        with patch.object(requests, 'get') as get_page_mock:
+            get_page_mock.return_value.status_code = 200
+            get_page_mock.return_value.text = "Success !"
+
+            page_data = get_page("/pokedex/pikachu")
+
+            ## Assert that requests was called with the correct url
+            get_page_mock.assert_called_with("https://pokemondb.net/pokedex/pikachu")
+
+            self.assertEqual(page_data, "Success !")
+
+    def test_get_page_not_found(self):
+
+        with patch.object(requests, 'get') as get_page_mock:
+
+            get_page_mock.return_value.status_code = 404
+
+            with self.assertRaises(RequestError) as context:
+                page_data = get_page("/pokedex/lkerjewkjek")
+
+            self.assertEqual(context.exception.error_code, 404)
+            self.assertEqual(context.exception.error_message, "Page not found.")
+
+    def test_get_page_not_found(self):
+
+        with patch.object(requests, 'get') as get_page_mock:
+
+            get_page_mock.return_value.status_code = 503
+
+            with self.assertRaises(RequestError) as context:
+                page_data = get_page("/pokedex/pikachu")
+
+            self.assertEqual(context.exception.error_code, 503)
+            self.assertEqual(context.exception.error_message, "Server Error.")
 
 if __name__ == "__main__":
     unittest.main()
