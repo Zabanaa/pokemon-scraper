578dcea5ec62b264787fba306fab1444a9836c2f
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-20 12:59:18 +0200

    raise exception if 404 and 500+

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 import requests
 import psycopg2
+from exceptions import RequestError
 from bs4 import BeautifulSoup
 
 POKEDEX_DOMAIN  = "https://pokemondb.net"
@@ -80,7 +81,18 @@ def insert_to_db(db, pokemon):
         sys.exit(e)
 
 def get_page(url):
+
     resp    = requests.get("{}{}".format(POKEDEX_DOMAIN, url))
+    status_code = resp.status_code
+
+    if status_code != 200:
+
+        if status_code >= 500 and status_code <= 599 :
+            raise RequestError(status_code, "Server Error.")
+
+        if status_code == 404:
+            raise RequestError(status_code, "Page not found.")
+
     return resp.text
 
 def make_soup(html):
